c8fce6505340545ea4de6880b8e19e5921eb5f95
Devon Powell <Devon.Powell@D2L.com>
2021-01-20 10:10:22 -0500

    US124277 - Fix up languages
    This adds missing languages and also adds language verification.

--- build/lang/ar.js
+++ build/lang/ar.js
@@ -3,42 +3,45 @@ window.D2L = window.D2L || {};
 window.D2L.PolymerBehaviors = window.D2L.PolymerBehaviors || {};
 window.D2L.PolymerBehaviors.OutcomesUserProgress = window.D2L.PolymerBehaviors.OutcomesUserProgress || {};
 window.D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior = window.D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior || {};
+window.D2L.OutcomesUserProgress = window.D2L.OutcomesUserProgress || {};
+window.D2L.OutcomesUserProgress.Language = window.D2L.OutcomesUserProgress.Language || {};
+window.D2L.OutcomesUserProgress.Language.Ar = {
+	'a11yCollapsed': 'مطوي',
+	'a11yExpanded': 'موسّع',
+	'bigTrendAttemptsScreenReaderString': '{numAttempts, plural, =1 {محاولة واحدة {attemptNames}} other {من المحاولات {attemptNames} و{lastAttemptName}}}',
+	'bigTrendAttemptsTooltipString': '{numAttempts, plural, =1 {محاولة واحدة} other {من المحاولات}} {attemptNames}',
+	'close': 'إغلاق',
+	'evidence': 'الدليل',
+	'headingDate': 'التاريخ',
+	'headingEvidence': 'اسم الدليل',
+	'headingLoa': 'مستوى التحصيل',
+	'miniTrendAttemptsTooltipString': '{numAttempts, plural, =1 {محاولة واحدة} other {من المحاولات}} {attemptNames}',
+	'miniTrendScreenReaderText': 'تم التقييم {numAssessed} {numAssessed, plural, =1 {مرة واحدة} other {من المرات}}: {levelNames}',
+	'nodeAriaTextGroup': 'المجموعة {state}',
+	'nodeAriaTextLevel': 'المستوى {level}',
+	'nodeAriaTextPosition': '{position} من أصل {count}',
+	'noEvidence': 'لم يتم تقييم {outcome, select, competencies {الكفاءة} expectations {التوقع} objectives {الهدف} outcomes {النتيجة} standards {المعيار} other {المعيار}} هذا بعد',
+	'noOutcomesInstructor': 'لبدء تعقّب تحصيل {outcome, select, competencies {الكفاءات} expectations {التوقعات} objectives {الأهداف} outcomes {النتائج} standards {المعايير} other {المعايير}}، يجب محاذاة {outcome, select, competencies {الكفاءات} expectations {التوقعات} objectives {الأهداف} outcomes {النتائج} standards {المعايير} other {المعايير}} الخاصة بالمقرر التعليمي مع الأنشطة التي تم تقييمها.',
+	'noOutcomesStudent': 'لم يتم تقييم أي {outcome, select, competencies {كفاءات} expectations {توقعات} objectives {أهداف} outcomes {نتائج} standards {معايير} other {معايير}}.',
+	'noScaleInstructor': 'لعرض تقدم المتعلّم لتحقيق {outcome, select, competencies {الكفاءة} expectations {التوقع} objectives {الهدف} outcomes {النتيجة} standards {المعيار} other {المعيار}} هذا، يجب إنشاء مقياس تحصيل وتحديده.',
+	'noScaleStudent': 'لم يتم تعيين أي مقياس لقياس تحقيق {outcome, select, competencies {الكفاءات} expectations {التوقعات} objectives {الأهداف} outcomes {النتائج} standards {المعايير} other {المعايير}}.',
+	'noSearchResults': 'لم يتم العثور على أي نتائج للمصطلح "{searchTerm}"',
+	'notAssessed': 'لم يتم التقييم',
+	'numSearchResults': '{numResults} من عمليات البحث {numResults, plural, =1 {نتيجة} other {نتائج}} للمصطلح "{searchTerm}"',
+	'outcomesListDescription': 'قائمة {outcome, select, competencies {الكفاءات} expectations {التوقعات} objectives {الأهداف} outcomes {النتائج} standards {المعايير} other {المعايير}}',
+	'outcomesListLoading': 'يتم الآن التحميل',
+	'outcomesListLoadingComplete': 'اكتمل التحميل',
+	'searchCleared': 'تم مسح نتائج البحث',
+	'searchHint': 'البحث...',
+	'searchLabel': 'بحث',
+	'trend': 'الاتجاه',
+	'untitled': 'بلا عنوان'
+};
 
 /*
  * Ar lang terms
  * @polymerBehavior D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior.LangArBehavior
  */
 D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior.LangArBehavior = {
-	ar: {
-		'a11yCollapsed': 'مطوي',
-		'a11yExpanded': 'موسّع',
-		'bigTrendAttemptsScreenReaderString': '{numAttempts, plural, =1 {محاولة واحدة {attemptNames}} other {من المحاولات {attemptNames} و{lastAttemptName}}}',
-		'bigTrendAttemptsTooltipString': '{numAttempts, plural, =1 {محاولة واحدة} other {من المحاولات}} {attemptNames}',
-		'close': 'إغلاق',
-		'evidence': 'الدليل',
-		'headingDate': 'التاريخ',
-		'headingEvidence': 'اسم الدليل',
-		'headingLoa': 'مستوى التحصيل',
-		'miniTrendAttemptsTooltipString': '{numAttempts, plural, =1 {محاولة واحدة} other {من المحاولات}} {attemptNames}',
-		'miniTrendScreenReaderText': 'تم التقييم {numAssessed} {numAssessed, plural, =1 {مرة واحدة} other {من المرات}}: {levelNames}',
-		'noEvidence': 'لم يتم تقييم {outcome, select, competencies {الكفاءة} expectations {التوقع} objectives {الهدف} outcomes {النتيجة} standards {المعيار} other {المعيار}} هذا بعد',
-		'noOutcomesInstructor': 'لبدء تعقّب تحصيل {outcome, select, competencies {الكفاءات} expectations {التوقعات} objectives {الأهداف} outcomes {النتائج} standards {المعايير} other {المعايير}}، يجب محاذاة {outcome, select, competencies {الكفاءات} expectations {التوقعات} objectives {الأهداف} outcomes {النتائج} standards {المعايير} other {المعايير}} الخاصة بالمقرر التعليمي مع الأنشطة التي تم تقييمها.',
-		'noOutcomesStudent': 'لم يتم تقييم أي {outcome, select, competencies {كفاءات} expectations {توقعات} objectives {أهداف} outcomes {نتائج} standards {معايير} other {معايير}}.',
-		'noScaleInstructor': 'لعرض تقدم المتعلّم لتحقيق {outcome, select, competencies {الكفاءة} expectations {التوقع} objectives {الهدف} outcomes {النتيجة} standards {المعيار} other {المعيار}} هذا، يجب إنشاء مقياس تحصيل وتحديده.',
-		'noScaleStudent': 'لم يتم تعيين أي مقياس لقياس تحقيق {outcome, select, competencies {الكفاءات} expectations {التوقعات} objectives {الأهداف} outcomes {النتائج} standards {المعايير} other {المعايير}}.',
-		'noSearchResults': 'لم يتم العثور على أي نتائج للمصطلح "{searchTerm}"',
-		'nodeAriaTextGroup': 'المجموعة {state}',
-		'nodeAriaTextLevel': 'المستوى {level}',
-		'nodeAriaTextPosition': '{position} من أصل {count}',
-		'notAssessed': 'لم يتم التقييم',
-		'numSearchResults': '{numResults} من عمليات البحث {numResults, plural, =1 {نتيجة} other {نتائج}} للمصطلح "{searchTerm}"',
-		'outcomesListDescription': 'قائمة {outcome, select, competencies {الكفاءات} expectations {التوقعات} objectives {الأهداف} outcomes {النتائج} standards {المعايير} other {المعايير}}',
-		'outcomesListLoading': 'يتم الآن التحميل',
-		'outcomesListLoadingComplete': 'اكتمل التحميل',
-		'searchCleared': 'تم مسح نتائج البحث',
-		'searchHint': 'البحث...',
-		'searchLabel': 'بحث',
-		'trend': 'الاتجاه',
-		'untitled': 'بلا عنوان'
-	}
+	ar: window.D2L.OutcomesUserProgress.Language.Ar
 };
--- src/localize-behavior.js
+++ src/localize-behavior.js
@@ -1,8 +1,12 @@
 import 'd2l-localize-behavior/d2l-localize-behavior.js';
 import '../build/lang/ar.js';
+import '../build/lang/cy.js';
+import '../build/lang/da.js';
 import '../build/lang/de.js';
 import '../build/lang/en.js';
+import '../build/lang/es-es.js';
 import '../build/lang/es.js';
+import '../build/lang/fr-fr.js';
 import '../build/lang/fr.js';
 import '../build/lang/ja.js';
 import '../build/lang/ko.js';
@@ -17,14 +21,27 @@ window.D2L.PolymerBehaviors = window.D2L.PolymerBehaviors || {};
 window.D2L.PolymerBehaviors.OutcomesUserProgress = window.D2L.PolymerBehaviors.OutcomesUserProgress || {};
 /** @polymerBehavior D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior */
 D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehaviorImpl = {
+	locale: {
+		type: String,
+		value: function() {
+			var locale = document.documentElement.lang
+				|| document.documentElement.getAttribute('data-lang-default')
+				|| 'en-us';
+			return locale.toLowerCase();
+		}
+	},
 	properties: {
 		resources: {
 			value: function() {
 				return {
 					'ar': this.ar,
+					'cy': this.cy,
+					'da': this.da,
 					'de': this.de,
 					'en': this.en,
+					'es-es': this.esEs,
 					'es': this.es,
+					'fr-fr': this.frFr,
 					'fr': this.fr,
 					'ja': this.ja,
 					'ko': this.ko,
@@ -32,21 +49,25 @@ D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehaviorImpl = {
 					'pt': this.pt,
 					'sv': this.sv,
 					'tr': this.tr,
-					'zh': this.zh,
-					'zh-TW': this.zhTw
+					'zh-tw': this.zhTw,
+					'zh': this.zh
 				};
 			}
 		}
 	}
 };
 /** @polymerBehavior */
-D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior = [
+window.D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior = [
 	D2L.PolymerBehaviors.LocalizeBehavior,
 	D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehaviorImpl,
 	D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior.LangArBehavior,
+	D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior.LangCyBehavior,
+	D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior.LangDaBehavior,
 	D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior.LangDeBehavior,
 	D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior.LangEnBehavior,
+	D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior.LangEsEsBehavior,
 	D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior.LangEsBehavior,
+	D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior.LangFrFrBehavior,
 	D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior.LangFrBehavior,
 	D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior.LangJaBehavior,
 	D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior.LangKoBehavior,
@@ -54,6 +75,6 @@ D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior = [
 	D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior.LangPtBehavior,
 	D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior.LangSvBehavior,
 	D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior.LangTrBehavior,
-	D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior.LangZhBehavior,
-	D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior.LangZhTwBehavior
+	D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior.LangZhTwBehavior,
+	D2L.PolymerBehaviors.OutcomesUserProgress.LocalizeBehavior.LangZhBehavior
 ];
